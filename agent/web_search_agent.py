bb46e8e05cc4adda59a423198dc9d68be8fa883c
ajeetkumarAI <ajeetkumar.kumar.4434@gmail.com>
2026-02-08 17:14:20 +0530

    Update web_search_agent.py

--- agent/web_search_agent.py
+++ agent/web_search_agent.py
@@ -6,7 +6,7 @@ import os
 load_dotenv()
 
 web_search_agent = Agent(
-    model=Groq(id="llama-3.3-70b-versatile"),
+    model=Groq(id="meta-llama/llama-4-scout-17b-16e-instruct"),
     tools=[TavilyTools(api_key=os.getenv("TAVILY_API_KEY"))],
     description="You are a web research agent. Search the web and return key facts and info.",
 )
@@ -14,3 +14,13 @@ web_search_agent = Agent(
 def search(query):
     result = web_search_agent.run(query)
     return getattr(result, 'content', str(result))
+
+# web_search_agent = Agent(
+#     model=Groq(id="llama-3.3-70b-versatile"),
+#     tools=[TavilyTools(api_key=os.getenv("TAVILY_API_KEY"))],
+#     description="You are a web research agent. Search the web and return key facts and info.",
+# )
+
+# def search(query):
+#     result = web_search_agent.run(query)
+#     return getattr(result, 'content', str(result))
